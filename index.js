b65304e923d2d828a11e35d69d59b9d18c8ac8e7
mopaul873 <157943180+mopaul873@users.noreply.github.com>
2025-05-15 03:50:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const api_root="https://adpnfuga4f.execute-api.us-west-2.amazonaws.com/register2/"
+const api_root="https://pdt39wgrye.execute-api.us-east-1.amazonaws.com/register"
 function loadTable(){
     const xhttp = new XMLHttpRequest();
     xhttp.open("GET",api_root + "users");
